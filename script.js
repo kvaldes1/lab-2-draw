27c47e847d99dffc0d247a655586cf56d375c0d0
kvaldes1 <156938722+kvaldes1@users.noreply.github.com>
2024-01-25 21:27:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function setup(){
 }
 
 function draw(){
+  eclipse(235, 228, 21)
   rect(150,200,200,200)
   triange(150,200,250,100,350,200)
+  fill(21, 235, 60)
   rect(200,250,50,150)
   rect(180,220,50,50)
   rect(270,220,50,50)
