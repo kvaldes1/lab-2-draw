ea26b48ba7868fae4b720ffd534c43f6e1ffaeba
kvaldes1 <156938722+kvaldes1@users.noreply.github.com>
2024-01-25 21:18:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function setup(){
 }
 
 function draw(){
-  background(200);
-  fill(235, 185, 21)
-  circle(50,50,50)
+  rect(150,200,200,200)
+  triange(150,200,250,100,350,200)
+  rect(200,250,50,150)
+  rect(180,220,50,50)
+  rect(270,220,50,50)
   
   // add your drawing code here ...
   
